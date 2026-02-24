ee9f926bf6648bcbf4eb485c16ff3cf07fa9ec5f
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-02-24 13:51:49 +0530

    feat : implement yargs

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { LLMClient } from "./client/llmClient.js";
 
 
-async function main(){
-    const client = new LLMClient()
-    const messages = [
+async function main() {
+  yargs(hideBin(process.argv)).command(
+    "$0 [prompt]",
+    "Run chat",
+    () => {},
+    async (argv) => {
+      const client = new LLMClient();
+      const messages = [
         {
-            role: "user",
-            content: "Hii"
-        }
-    ]
-    // await client.chatCompletion(messages, false)
-    for await (const event of client.chatCompletion(messages, true)) {
-        console.log(JSON.stringify(event))
+          role: "user",
+          content: argv.prompt,
+        },
+      ];
+      // await client.chatCompletion(messages, false)
+      for await (const event of client.chatCompletion(messages, true)) {
+        console.log(JSON.stringify(event));
+      }
+      console.log("Done");
     }
-    console.log("Done")
+  )
+  .parse()
 }
 
-main()
+main();
